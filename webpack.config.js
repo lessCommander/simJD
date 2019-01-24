63444a209733b2b72df5a03f9f9c17534fd1c9a4
ls <2310975964@qq.com>
2019-01-24 22:18:01 +0800

    add header-nav

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 })
             },
             {
-                test: /\.(png|jpg)$/,
+                test: /\.(png|jpg|gif)$/,
 　　　　　　     loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
             },
             {
